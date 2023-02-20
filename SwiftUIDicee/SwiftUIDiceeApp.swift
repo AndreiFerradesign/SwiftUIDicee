edc32a6d8ee081efb85c72fce4c0b3ad1510d785
AndreiFerradesign <ferradesign@yandex.ru>
2023-02-20 09:55:07 +0100

    Initial Commit

---
+++ SwiftUIDicee/SwiftUIDiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDiceeApp.swift
+//  SwiftUIDicee
+//
+//  Created by Andrei on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
